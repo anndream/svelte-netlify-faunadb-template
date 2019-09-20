e23bfdb5a63fe59c813cd0768baaea4b88fe5fba
TheSacredlipton <44257207+TheSacredLipton@users.noreply.github.com>
2019-09-20 20:42:42 +0900

    svelte変更うまくいった

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+  const name = "World";
+  fetch("./.netlify/functions/hello", {
+    mode: "cors"
+  })
+    .then(response => response.text())
+    .then(text => console.log(text))
+    .catch(error => console.log(error));
+
+  // Function using fetch to POST to our API endpoint
+  function createTodo(data) {
+    return fetch("./.netlify/functions/todos-create", {
+      body: JSON.stringify(data),
+      method: "POST"
+    }).then(response => {
+      return response.json();
+    });
+  }
+
+  // Todo data
+  const myTodo = {
+    title: "My todo title",
+    completed: false
+  };
+
+  // create it!
+  createTodo(myTodo)
+    .then(response => {
+      console.log("API response", response);
+      // set app state
+    })
+    .catch(error => {
+      console.log("API error", error);
+    });    
+</script>
+
+<style>
+  h1 {
+    color: #212121;
+  }
+</style>
+
+<h1>Hello {name}!</h1>
